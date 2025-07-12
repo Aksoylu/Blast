ef3d72b0650dad9e518b84dba1a89e2fa50fecee
Ümit Aksoylu <umitaksoylu98@gmail.com>
2025-07-12 10:23:42 +0300

    feat:state protection implementation for main layout

--- Client/react-ui/src/Pages/Layout.tsx
+++ Client/react-ui/src/Pages/Layout.tsx
@@ -8,7 +8,7 @@ import {
   useDisclosure,
   BoxProps
 } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 import { AnimatePresence } from 'framer-motion'
 import { motion } from 'framer-motion'
@@ -18,15 +18,41 @@ import { Teams } from './Teams'
 import { Settings } from './Settings'
 import { Configurations } from './Configurations'
 
-import { SideMenu,Navbar } from '#/Components';
+import { SideMenu, Navbar } from '#/Components';
 
 import { Pages } from '#/Enums'
 
 export const Layout = () => {
+  const layoutRef = useRef<HTMLDivElement>(null);
+
+  const [layoutHeight, setLayoutHeight] = useState<number | null>(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [activePage, setActivePage] = useState<Pages>(Pages.Home);
 
-  const MotionBox = motion<BoxProps>(Box)
+  const pages = {
+    [Pages.Home]: Home,
+    [Pages.Teams]: Teams,
+    [Pages.Configurations]: Configurations,
+    [Pages.Settings]: Settings,
+  };
+
+  // #region UI Functions
+  const updateHeight = () => {
+    const topOffset = layoutRef.current?.getBoundingClientRect().top || 0;
+    const windowHeight = window.innerHeight - 10;
+    const newLayoutHeight = windowHeight - topOffset;
+    setLayoutHeight(newLayoutHeight);
+  };
+  //#endregion
+
+  //#region UI Hooks
+  useLayoutEffect(() => {
+    updateHeight();
+    window.addEventListener('resize', updateHeight);
+    return () => window.removeEventListener('resize', updateHeight);
+  }, []);
+  //#endregion
+
 
   return (
     <Box minH="100vh" overflow="hidden" bg={useColorModeValue('gray.100', 'gray.900')}>
@@ -48,21 +74,32 @@ export const Layout = () => {
       </Drawer>
       <Navbar onOpen={onOpen} />
 
-      <Box ml={{ base: 0, md: 40 }} p={2} height="100%" overflow="hidden">
-        <AnimatePresence mode="wait">
-          <MotionBox
-            key={activePage}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3 }}
-          >
-            {activePage === Pages.Home && <Home />}
-            {activePage === Pages.Teams && <Teams />}
-            {activePage === Pages.Configurations && <Configurations />}
-            {activePage === Pages.Settings && <Settings />}
-          </MotionBox>
-        </AnimatePresence>
+      <Box ml={{ base: 0, md: 40 }} p={2} overflow="hidden" ref={layoutRef} height={`${layoutHeight}px`} position="relative">
+        {Object.entries(pages).map(([key, PageComponent]) => {
+          const isActive = key === activePage.toString();
+
+          return (
+            <motion.div
+              key={key}
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                top: 0,
+                left: 0,
+                display: "block",
+                pointerEvents: isActive ? "auto" : "none",
+                zIndex: isActive ? 1 : 0,
+              }}
+              animate={{
+                opacity: isActive ? 1 : 0,
+              }}
+              transition={{ duration: 0.3 }}
+            >
+              <PageComponent />
+            </motion.div>
+          );
+        })}
       </Box>
     </Box>
   )
